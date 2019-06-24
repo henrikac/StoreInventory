1bc8331c73db685da54d0d78195026b4a29ea0df
Henrik Christensen <sensen1695@hotmail.com>
2019-06-24 15:51:44 +0200

    added type hints + added a function to add a single product to the database

--- src/product.py
+++ src/product.py
@@ -5,6 +5,7 @@ because peewee does not support type hinting
 """
 
 from datetime import datetime
+from typing import Dict, List, Union
 
 from peewee import *
 from playhouse.shortcuts import model_to_dict
@@ -13,37 +14,45 @@ from playhouse.shortcuts import model_to_dict
 db = SqliteDatabase('inventory.db')
 
 
-def open_db():
+def open_db() -> None:
     """Opens the database connection and creates the tables"""
     db.connect()
     db.create_tables([Product], safe=True)
 
 
-def close_db():
+def close_db() -> None:
     """Closes the database connection"""
     db.close()
 
 
-def add_products_to_db(products):
-    """Adds products to the database.
+def add_products_to_db(products: List[Dict]) -> None:
+    """Adds a list of products to the database.
     If a 'product_name' already exists in the database
     the most recent updated version will be saved to the database
     """
     with db.atomic():
         for product in products:
-            try:
-                Product.create(**product)
-            except IntegrityError:
-                old_product = Product.get(product_name=product['product_name'])
-
-                if old_product.date_updated < product['date_updated']:
-                    old_product.product_price = product['product_price']
-                    old_product.product_quantity = product['product_quantity']
-                    old_product.date_updated = product['date_updated']
-                    old_product.save()
+            add_product_to_db(product)
 
 
-def get_product(id):
+def add_product_to_db(product: Dict) -> None:
+    """Adds a single product to the database
+    If a 'product_name' already exists in the database
+    the most recent updated version wil be saved to the database
+    """
+    try:
+        Product.create(**product)
+    except IntegrityError:
+        old_product = Product.get(product_name=product['product_name'])
+
+        if old_product.date_updated < product['date_updated']:
+            old_product.product_price = product['product_price']
+            old_product.product_quantity = product['product_quantity']
+            old_product.date_updated = product['date_updated']
+            old_product.save()
+
+
+def get_product(id: int) -> Union[Dict, None]:
     """Gets a product from the database by its id
     Returns None if no product with given id
     """
